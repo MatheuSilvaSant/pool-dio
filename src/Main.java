1b67a323692dec5ce40131e89053471573f992bb
MatheusSilv <matheu.lpp@gmail.com>
2022-07-23 21:05:01 -0300

    Abstracao e encapsulamento

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args){
+        Curso curso1 = new Curso();
+       curso1.setDescription("Curso em java");
+       curso1.setCargaHoraria(8);
+       curso1.setTitle("Java");
+
+        Mentoria men1 = new Mentoria();
+        men1.setTitle("Mentoria java");
+        men1.setDescription("descrição java");
+        men1.setDate(LocalDate.now());
+
+
+
+        Curso curso2 = new Curso();
+        curso2.setTitle("Python");
+        curso2.setDescription("Curso em python");
+        curso2.setCargaHoraria(10);
+
+       System.out.println(curso1 +" \n"+curso2 + " \n" + men1);
+    }
+}
